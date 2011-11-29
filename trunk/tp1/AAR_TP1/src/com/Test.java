874da712e828a66408e5011968635d46c6c5a64e
imouctar@gmail.com <imouctar@gmail.com@c4080868-7abf-2f7f-b321-b1512ac3d24a>
2011-11-29 10:49:12 +0000

    select marche

--- trunk/tp1/AAR_TP1/src/com/Test.java
+++ trunk/tp1/AAR_TP1/src/com/Test.java
@@ -1,10 +1,14 @@
 package com;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.aar.util.HibernateUtil;
-import com.aar.velib.Station;
+import com.aar.velib.RealStation;
 
 public class Test {
 
@@ -17,20 +21,20 @@ public class Test {
 		Transaction transaction = null;
 		try {
 			transaction = session.beginTransaction();
-			// Query query = session.createQuery("from realstation");
+			Query query = session.createQuery("from RealStation");
 			// query.setMaxResults(10);
-			// List Stations = query.list();
-			//
-			// for (Iterator iter = Stations.iterator(); iter.hasNext();) {
-			// RealStation s = (RealStation) iter.next();
-			// System.out.println(s.getVelibID());
-			// }
+			List Stations = query.list();
 
-			Station s = new Station();
-			s.setVelibId(1234);
-			s.setAddress("test adress");
-			s.setVersion(10);
-			session.save(s);
+			for (Iterator iter = Stations.iterator(); iter.hasNext();) {
+				RealStation s = (RealStation) iter.next();
+				System.out.println(s.getVelibID());
+			}
+
+			// Station s = new Station();
+			// s.setVelibId(10001);
+			// s.setAddress("test adress2");
+			// s.setVersion(10);
+			// session.save(s);
 			transaction.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
--- trunk/tp1/AAR_TP1/src/com/aar/util/HibernateUtil.java
+++ trunk/tp1/AAR_TP1/src/com/aar/util/HibernateUtil.java
@@ -5,20 +5,37 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	private static final SessionFactory session;
-	static {
-		try {
-			session = new Configuration().configure().buildSessionFactory();
-		} catch (Throwable e) {
+	private static final SessionFactory sessionFactory = buildSessionFactory();
 
-			System.out
-					.println("probleme lors de l'initialisation de la session: "
-							+ e.getMessage());
-			throw new ExceptionInInitializerError(e);
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			return new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
 		}
 	}
 
 	public static SessionFactory getSessionFactory() {
-		return session;
+		return sessionFactory;
 	}
+
+	// private static final SessionFactory session;
+	// static {
+	// try {
+	// session = new Configuration().configure().buildSessionFactory();
+	// } catch (Throwable e) {
+	//
+	// System.out
+	// .println("probleme lors de l'initialisation de la session: "
+	// + e.getMessage());
+	// throw new ExceptionInInitializerError(e);
+	// }
+	// }
+	//
+	// public static SessionFactory getSessionFactory() {
+	// return session;
+	// }
 }
