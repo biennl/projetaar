d3ca13a7e5dc3ff1fd96ff6a48bc16b2151528ea
imouctar@gmail.com <imouctar@gmail.com@c4080868-7abf-2f7f-b321-b1512ac3d24a>
2011-11-29 09:40:22 +0000

    rien

--- trunk/tp1/AAR_TP1/src/com/Test.java
+++ trunk/tp1/AAR_TP1/src/com/Test.java
@@ -1,14 +1,10 @@
 package com;
 
-import java.util.Iterator;
-import java.util.List;
-
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.aar.util.HibernateUtil;
-import com.aar.velib.RealStation;
+import com.aar.velib.Station;
 
 public class Test {
 
@@ -21,14 +17,20 @@ public class Test {
 		Transaction transaction = null;
 		try {
 			transaction = session.beginTransaction();
-			Query query = session.createQuery("from realstation");
-			query.setMaxResults(10);
-			List Stations = query.list();
+			// Query query = session.createQuery("from realstation");
+			// query.setMaxResults(10);
+			// List Stations = query.list();
+			//
+			// for (Iterator iter = Stations.iterator(); iter.hasNext();) {
+			// RealStation s = (RealStation) iter.next();
+			// System.out.println(s.getVelibID());
+			// }
 
-			for (Iterator iter = Stations.iterator(); iter.hasNext();) {
-				RealStation s = (RealStation) iter.next();
-				System.out.println(s.getVelibID());
-			}
+			Station s = new Station();
+			s.setVelibId(1234);
+			s.setAddress("test adress");
+			s.setVersion(10);
+			session.save(s);
 			transaction.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
